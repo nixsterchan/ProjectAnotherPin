71a7ce057be692398892220e6b20712320bcbb55
Nixster <nigel_chan@mymail.sutd.edu.sg>
2018-10-26 16:50:44 +0800

    Added cart functionality and linked to order

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  # sets it up on every single part of our website
+  before_action :current_cart
+
+  # lets us put it into views 
+  helper_method :current_cart
+
+  def current_cart
+    # if customer just arrived on the site, give brand new cart
+    # if a cart already exists for them, keep it there
+    # check session[:cart_id]
+
+    if session[:cart_id].present?
+      # already have a cart 
+      @current_cart = Cart.find(session[:cart_id])
+    else
+      # do not have a cart
+      @current_cart = Cart.create
+      # creates the cart for every page
+      session[:cart_id] = @current_cart.id 
+    end
+  end
 end
---
+++ db/migrate/20181026075519_add_cart_to_model_items.rb
@@ -0,0 +1,7 @@
+class AddCartToModelItems < ActiveRecord::Migration[5.2]
+  def change
+
+    add_column :order_items, :cart_id, :integer
+    
+  end
+end
